246a90f8e5eb5906585a76d85db94b97b03348e7
dhp14@inf.ufpr.br <dhp14@inf.ufpr.br>
2016-05-19 11:09:28 -0300

    adicionado comentarios zueiros

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import ponto.py
 
+#que codigo bonito meu amigo
 #comentario qlq
 main:
 	#print este e um supermercado
 	
 
 
-caixa:
+caixa:#que gambiarra kkkk
 	#controlar esteira
 	#ler codigo de barras do produtos
 	#calcular preco de cada produto	
